3097e4b7bf4322206cb8f1e3ec1dda4d17892fa7
Wyatt <wyatthalsa@gmail.com>
2025-11-25 01:33:32 +0000

    GeometryApp

--- sphere.py
+++ sphere.py
@@ -1,10 +1,10 @@
-# inspiration code for Python Unit Testing Project
+import math
 
-def surfaceArea():
-    pass
+def surfaceArea(radius):
+    return 4 * math.pi * radius * radius
 
-def volume():
-    pass
+def volume(radius):
+    return (4/3) * math.pi * (radius ** 3)
 
 def prompt():
     print()
@@ -12,6 +12,7 @@ def prompt():
     print("PYTHON PROGRAM TO FIND THE VOLUME OF A SPHERE")
     print("------------------------------------------------------------")
     radius = int(input("Please Enter the radius :"))
+    print("Volume:", volume(radius))
 
 if __name__ == '__main__':
     prompt()
--- spheretest.py
+++ spheretest.py
@@ -1,22 +1,22 @@
 import unittest
 import sphere
+import math
 
 class sphereTest(unittest.TestCase):
 
     #these need to be updated for a sphere
     def test_volume1(self):
-        assert(sphere.volume() == ??)
+        assert( sphere.volume(1) == (4/3) * math.pi * (1**3) )
 
     def test_volume2(self):
-        assert(sphere.volume() == ??)
+        assert( sphere.volume(2) == (4/3) * math.pi * (2**3) )
 
     def test_volume3(self):
-        assert(sphere.volume() == ??)
+        assert( sphere.volume(3) == (4/3) * math.pi * (3**3) )
 
-    #failing test
-    #def test_volume3(self):
-        #assert(sphere.volume(10,1000) == ??)
-
+    #failing test example (radius + height does NOT apply to spheres)
+    #def test_volume4(self):
+    #    assert( sphere.volume(10,1000) == ?? )
 
 if __name__ == '__main__':
     unittest.main()
